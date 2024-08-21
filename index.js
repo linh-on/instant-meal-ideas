4f8d879e57eeb7aa3039e50a3c62cdb3861e43c1
Linh On <lmon@uci.edu>
2024-08-21 20:30:51 +0700

    Set up website to search for meals when input one ingredient

--- index.js
+++ index.js
@@ -8,9 +8,27 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 let recipes = [];
 
+const THE_MEAL_DB_URL = "https://www.themealdb.com/api/json/v1/1/filter.php?i=";
+
 //home page
 app.get("/", (req, res) => {
   res.render("index.ejs", { recipes: recipes });
+});
+
+app.post("/recipe", async (req, res) => {
+  try {
+    const ingredient = req.body.ingredients;
+    const response = await axios.get(
+      `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`
+    );
+    const result = response.data;
+    const meals = response.data.meals;
+    console.log(result);
+    res.render("recipe.ejs", { meals: meals, ingredient: ingredient });
+  } catch (error) {
+    console.error(error);
+    res.render("recipe.ejs", { meals: null, ingredient: ingredient });
+  }
 });
 
 app.listen(port, () => {
